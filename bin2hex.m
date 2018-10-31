90dc4ea1d889cf278fbd30b267252a7617c80156
Daniel Neunteufel <daniel.neunteufel@tuwien.ac.at>
2018-10-31 16:20:17 +0100

    0x/0b prefix functionality added to hex2bin/bin2hex

--- bin2hex.m
+++ bin2hex.m
@@ -1,11 +1,19 @@
 function hex = bin2hex(bin,n)
 
+    if strcmpi(bin(1:2),'0b')
+        bin(1:2) = '';
+    end
+
+    if strcmpi(bin(1),'b')
+        bin(1) = '';
+    end
+
     dec = bin2dec(bin);
-    
+
     if nargin == 2
         hex = dec2hex(dec,n);
     else
         hex = dec2hex(dec);
     end
-    
+
 end
--- hex2bin.m
+++ hex2bin.m
@@ -1,5 +1,13 @@
 function bin = hex2bin(hex,n)
 
+    if strcmpi(hex(1:2),'0x')
+        hex(1:2) = '';
+    end
+
+    if strcmpi(hex(1),'x')
+        hex(1) = '';
+    end
+
     dec = hex2dec(hex);
 
     if nargin == 2
