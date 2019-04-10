7769f91d14a4e6dfc846e78afa7362136d2a531e
Daniel Neunteufel <daniel.neunteufel@tuwien.ac.at>
2019-04-10 16:39:08 +0200

    init_tt_ff: ff only calculated when nargout requires to do so

--- +misc/init_tt_ff.m
+++ +misc/init_tt_ff.m
@@ -10,16 +10,20 @@ function [ tt , ff , full ] = init_tt_ff(N,fs)
     df = 1/T;
 
     tt = (0:dt:T).';
-    ff = (-fs/2:df:fs/2).' - mod(N-1,2)*df/2;
-
-    ff = ff - ff(abs(ff) == min(abs(ff)));
     
-    full.tt = tt;
-    full.ff = ff;
-    full.dt = dt;
-    full.df = df;
-    full.T = T;
-    full.fs = fs;
-    full.N = N;
-
+    if nargout > 1
+        ff = (-fs/2:df:fs/2).' - mod(N-1,2)*df/2;
+        ff = ff - ff(abs(ff) == min(abs(ff)));
+    end
+    
+    if nargout > 2
+        full.tt = tt;
+        full.ff = ff;
+        full.dt = dt;
+        full.df = df;
+        full.T = T;
+        full.fs = fs;
+        full.N = N;
+    end
+    
 end
