bd52493cbc45a3d4e8acca607cecc143b59798d2
Daniel Neunteufel <daniel.neunteufel@tuwien.ac.at>
2019-04-11 15:06:15 +0200

    list_folder_content rewritten to allow for file/folder filtering and absolute input pathes

--- +misc/list_folder_content.m
+++ +misc/list_folder_content.m
@@ -4,12 +4,22 @@ function filename_list = list_folder_content(location,varargin)
 % varargin (optional):
 %   'ignore_chars'    ... Files starting with characters in this char array
 %                         will be ignored. Default: '';
-%   'absolutepath'    ... Return absolute path instead of relative.
+%   'absolutepath'    ... Return absolute path instead of relative. If
+%                         false, try to get relative path. If this is not
+%                         possible, absolute path will be returned. (Sorry,
+%                         this is due to backward compatibility.)
 %                         Default: false
+%   'files_only'      ... Returns only files, no folders. Default: true
+%   'folders_only'    ... Returns only folders, no files. Default: false
+%                         (Except for '.' and '..'.)
+%                         Setting both files_only and folders_only true
+%                         will lead to an empty filename_list.
 %
 % Examples:
 % filename_list = list_folder_content('..','absolutepath',true); % Lists everything in parent folder using absolute paths.
 % filename_list = list_folder_content(ignore_chars,'.#'); % Lists current folder ignoring all files starting with '.' or '#'.
+
+%% parse input
 
     if nargin == 0
         location = [];
@@ -24,51 +34,44 @@ function filename_list = list_folder_content(location,varargin)
 
     p.addParameter('ignore_chars','',@(x) validateattributes(x,{'char','string'},{'scalartext'}));
     p.addParameter('absolutepath',false,@(x) validateattributes(boolean(x),{'logical'},{'scalar'}));
+    p.addParameter('files_only',true,@(x) validateattributes(boolean(x),{'logical'},{'scalar'}));
+    p.addParameter('folders_only',false,@(x) validateattributes(boolean(x),{'logical'},{'scalar'}));
 
     p.parse(varargin{:});
 
     ignore_chars = p.Results.ignore_chars;
     absolutepath = boolean(p.Results.absolutepath);
+    files_only = boolean(p.Results.files_only);
+    folders_only = boolean(p.Results.folders_only);
 
-%%
-%     if nargin < 1
-%         location = '.';
-%     end
-%     
-%     if nargin < 2
-%         chars_ignore = '';
-%     end
+%% prepare location
+
+    if isempty(location) % empty location or '.' or '..'
+        location = './';
+    elseif numel(location) == 1 && strcmp(location,'.') || numel(location) == 2 && strcmp(location,'..')
+        location = [location , filesep];
+    end
 
     location = strrep(location,'\',filesep);
     location = strrep(location,'/',filesep);
 
-    
-    assert(~strcmp(location(1),filesep) && ~ismember(':',location),'Use relative path.');
-
-    
-    if isempty(location)
-        location = '.';
-    end
-
-    if numel(location) > 1 && strcmp(location(end-1:end),[filesep,'.'])
-        location = location(1:end-1);
+    if numel(location) > 1 && strcmp(location(end-1:end),[filesep,'.']) % drop trailing period
+        location(end) = [];
     end
     
-    if ~ismember(location(end),'/\')
-        if exist(location,'file') == 7
-            location = [location , filesep];
+%% check regexp
+
+    if ~strcmp(location(end),filesep) % no trailing filesep -> no folder -> check for regexp
+        [location,regexpmask,suffix] = fileparts(location);
+        regexpmask = [regexpmask,suffix];
+        if isempty(location)
+            location = '.';
         end
-    end
-
-    [location,regexpmask,suffix] = fileparts(location);
-    regexpmask = [regexpmask,suffix];
-    if isempty(location)
-        location = ['.'];
-    end
-    if ~strcmp(location(end),filesep)
         location = [location , filesep];
+    else % location was folder all along
+        regexpmask = '';
+        suffix = '';
     end
-    assert(isfolder(location),'''%s'' is not a folder.',location);
 
     if isempty(regexpmask)
         regexpmask = '*';
@@ -77,22 +80,46 @@ function filename_list = list_folder_content(location,varargin)
     regexpmask = strrep(regexpmask,'*','.*');
     regexpmask = ['^',regexpmask,'$'];
 
+    regexpmask = [regexpmask , suffix];
+
+%% get location content
+
+    assert(isfolder(location),'Folder ''%s'' not found.',location);
     dircontent = dir(location);
 
-    idx_match_regexp = cellfun(@(x) ~isempty(regexp(x,regexpmask,'once')),{dircontent.name});
+    % remove '.' and '..'
+    dircontent = dircontent(~ismember({dircontent.name},{'.','..'}));
 
+%% filtering
+
+    % filter regexp
+    idx_match_regexp = cellfun(@(x) ~isempty(regexp(x,regexpmask,'once')),{dircontent.name});
     dircontent = dircontent(idx_match_regexp);
 
-    ignore_file = cellfun(@(x) ismember(x(1),ignore_chars),{dircontent.name});
-    if absolutepath
-        filename_list = cellfun(@(x) [dircontent(1).folder,filesep,x],{dircontent(~[dircontent.isdir] & ~ignore_file).name},'UniformOutput',false);        
-    else
-        if ~strcmp(location(1:2),['.',filesep])
-            location = ['.' , filesep , location];
+    % filter ignore character
+    idx_ignore_file = cellfun(@(x) ismember(x(1),ignore_chars),{dircontent.name});
+    dircontent = dircontent(~idx_ignore_file);
+
+    % filter files only
+    if files_only
+        dircontent = dircontent(~[dircontent.isdir]);
+    end
+
+    % filter folders only
+    if folders_only
+        dircontent = dircontent([dircontent.isdir]);
+    end
+
+%% build filename list
+
+    filename_list = cellfun(@(x) [dircontent(1).folder,filesep,x],{dircontent.name},'UniformOutput',false);
+
+    if ~isempty(filename_list) && ~absolutepath % get rid of pwd in file/folder names if possible
+        filename_list_old = filename_list;
+        filename_list = cellfun(@(x) strrep(x,pwd,'.'),filename_list,'UniformOutput',false);
+        if isequal(filename_list,filename_list_old)
+            warning('Cannot resolve relative path.');
         end
-        filename_list = cellfun(@(x) [location,x],{dircontent(~[dircontent.isdir] & ~ignore_file).name},'UniformOutput',false);
     end
 
 end
-
-
