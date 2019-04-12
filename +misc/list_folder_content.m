cc44dc09887977a2f87f6ac52b11703fd7fc6114
Daniel Neunteufel <daniel.neunteufel@tuwien.ac.at>
2019-04-12 10:23:47 +0200

    list_folder_content major changes: input parameter absolutepath changed to relativepath (default=false); files_only  not default=false; added relative path input functionality

--- +misc/list_folder_content.m
+++ +misc/list_folder_content.m
@@ -1,23 +1,31 @@
 function filename_list = list_folder_content(location,varargin)
 %filename_list = list_folder_content(location,varargin)
+%Returns 'name' property of the dir(location) result with some filters
+%applied. Folders '.' and '..' are always excluded. Regexps allowed.
+%
 % location (optional) ... Location to list. Must be folder. Default: '.';
 % varargin (optional):
 %   'ignore_chars'    ... Files starting with characters in this char array
 %                         will be ignored. Default: '';
-%   'absolutepath'    ... Return absolute path instead of relative. If
-%                         false, try to get relative path. If this is not
-%                         possible, absolute path will be returned. (Sorry,
-%                         this is due to backward compatibility.)
+%   'relativepath'    ... Try to return relative path instead of absolute.
+%                         If not possible, fall back to absolute.
 %                         Default: false
-%   'files_only'      ... Returns only files, no folders. Default: true
+%   'files_only'      ... Returns only files, no folders. Default: false
 %   'folders_only'    ... Returns only folders, no files. Default: false
-%                         (Except for '.' and '..'.)
 %                         Setting both files_only and folders_only true
 %                         will lead to an empty filename_list.
 %
 % Examples:
-% filename_list = list_folder_content('..','absolutepath',true); % Lists everything in parent folder using absolute paths.
-% filename_list = list_folder_content(ignore_chars,'.#'); % Lists current folder ignoring all files starting with '.' or '#'.
+% filename_list = misc.list_folder_content(); % Lists everything in current folder using absolute paths.
+% filename_list = misc.list_folder_content('..','folders_only',1,'relativepath',1); % Lists all folders in parent folder using relative paths.
+% filename_list = misc.list_folder_content('ignore_chars','.#'); % Lists current folder ignoring all files starting with '.' or '#'.
+
+%% defaults
+
+    ignore_chars_default = '';
+    relativepath_default = false;
+    files_only_default   = false;
+    folders_only_default = false;
 
 %% parse input
 
@@ -32,15 +40,15 @@ function filename_list = list_folder_content(location,varargin)
 
     p = inputParser;
 
-    p.addParameter('ignore_chars','',@(x) validateattributes(x,{'char','string'},{'scalartext'}));
-    p.addParameter('absolutepath',false,@(x) validateattributes(boolean(x),{'logical'},{'scalar'}));
-    p.addParameter('files_only',true,@(x) validateattributes(boolean(x),{'logical'},{'scalar'}));
-    p.addParameter('folders_only',false,@(x) validateattributes(boolean(x),{'logical'},{'scalar'}));
+    p.addParameter('ignore_chars'   ,ignore_chars_default   ,@(x) validateattributes(x,{'char','string'},{'scalartext'}));
+    p.addParameter('relativepath'   ,relativepath_default   ,@(x) validateattributes(boolean(x),{'logical'},{'scalar'}));
+    p.addParameter('files_only'     ,files_only_default     ,@(x) validateattributes(boolean(x),{'logical'},{'scalar'}));
+    p.addParameter('folders_only'   ,folders_only_default   ,@(x) validateattributes(boolean(x),{'logical'},{'scalar'}));
 
     p.parse(varargin{:});
 
     ignore_chars = p.Results.ignore_chars;
-    absolutepath = boolean(p.Results.absolutepath);
+    relativepath = boolean(p.Results.relativepath);
     files_only = boolean(p.Results.files_only);
     folders_only = boolean(p.Results.folders_only);
 
@@ -58,10 +66,10 @@ function filename_list = list_folder_content(location,varargin)
     if numel(location) > 1 && strcmp(location(end-1:end),[filesep,'.']) % drop trailing period
         location(end) = [];
     end
-    
+
 %% check regexp
 
-    if ~strcmp(location(end),filesep) % no trailing filesep -> no folder -> check for regexp
+    if ~isfolder(location) % no folder -> check for regexp
         [location,regexpmask,suffix] = fileparts(location);
         regexpmask = [regexpmask,suffix];
         if isempty(location)
@@ -80,12 +88,12 @@ function filename_list = list_folder_content(location,varargin)
     regexpmask = strrep(regexpmask,'*','.*');
     regexpmask = ['^',regexpmask,'$'];
 
-    regexpmask = [regexpmask , suffix];
+%     regexpmask = [regexpmask , suffix];
 
 %% get location content
 
     assert(isfolder(location),'Folder ''%s'' not found.',location);
-    dircontent = dir(location);
+    dircontent = dir([location,filesep]);
 
     % remove '.' and '..'
     dircontent = dircontent(~ismember({dircontent.name},{'.','..'}));
@@ -112,14 +120,46 @@ function filename_list = list_folder_content(location,varargin)
 
 %% build filename list
 
-    filename_list = cellfun(@(x) [dircontent(1).folder,filesep,x],{dircontent.name},'UniformOutput',false);
-
-    if ~isempty(filename_list) && ~absolutepath % get rid of pwd in file/folder names if possible
-        filename_list_old = filename_list;
-        filename_list = cellfun(@(x) strrep(x,pwd,'.'),filename_list,'UniformOutput',false);
-        if isequal(filename_list,filename_list_old)
-            warning('Cannot resolve relative path.');
+    if ~isempty(dircontent)
+        folder = dircontent(1).folder;
+        if relativepath
+            folder = get_relative_path(folder);
         end
+        if ~strcmp(folder(end),filesep)
+            folder = [folder,filesep];
+        end
+        filename_list = cellfun(@(x) [folder,x],{dircontent.name},'UniformOutput',false);
+    else
+        filename_list = {};
+    end
+
+end
+
+function folder_relative = get_relative_path(folder)
+
+    parts_folder = strsplit(lower([folder,filesep]),filesep);
+    parts_folder = parts_folder(cellfun(@(x) ~isempty(x),parts_folder));
+
+    parts_pwd = strsplit(lower(pwd),filesep);
+    parts_pwd = parts_pwd(cellfun(@(x) ~isempty(x),parts_pwd));
+
+    relative_possible = 0;
+    while ~isempty(parts_folder) && ...
+          ~isempty(parts_pwd) && ...
+          strcmpi(parts_folder{1},parts_pwd{1})
+
+        parts_folder(1) = [];
+        parts_pwd(1) = [];
+        relative_possible = 1;
+
+    end
+
+    if relative_possible
+        relative_raw = [repmat({'..'},1,numel(parts_pwd)),parts_folder];
+        folder_relative = ['.',filesep,strjoin(relative_raw,filesep)];
+    else
+        folder_relative = folder;
+        warning('Cannot resolve relative path. Using absolute instead.');
     end
 
 end
