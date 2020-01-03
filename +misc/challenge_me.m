cf01c7c32ae050319b5c713f80ca5f10f6183c42
Daniel Neunteufel <daniel.neunteufel@tuwien.ac.at>
2020-01-03 09:48:34 +0100

    fixed challenge_me

--- +misc/challenge_me.m
+++ +misc/challenge_me.m
@@ -1,4 +1,10 @@
-function status = challenge_me()
-    challenge_letter = char(96+randi(26));
-    status = all(challenge_letter == input(sprintf('For security reasons, please enter the letter ''%s'': ',challenge_letter),'s'));
+function status = challenge_me(level)
+    if nargin < 1
+        level = 1;
+    else
+        level = max(1,level);
+    end
+    challenge_letter = char(96+randi(26,1,level));
+    status = strcmp(challenge_letter,input(sprintf('For security reasons, please enter the letter(s) ''%s'': ',challenge_letter),'s'));
+    assert(status,'Consider being more careful next time!');
 end
