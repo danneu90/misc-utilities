fabc688d17ce0784066cacf65772a17839d8d770
Daniel Neunteufel <daniel.neunteufel@tuwien.ac.at>
2020-05-27 17:34:24 +0200

    githash cannot throw exceptions any more.

--- +misc/githash.m
+++ +misc/githash.m
@@ -3,24 +3,36 @@ function githash = githash(objname)
 %
 % Gets hash of current commit. If possible.
 
-    assert(ischar(objname) || isstring(objname),'''objname'' must be name of folder, file, or function in search path.');
-    if isfolder(objname)
-        D = dir(objname);
-        location_obj = D(1).folder;
-    else
-        location_obj = fileparts(which(objname));
-        assert(isfolder(location_obj),['Cmd "fileparts(which(''',objname,'''))" did not return a valid folder.' , newline , ...
-            'Returned "' , location_obj '" instead.']);
+    if nargin < 1
+        objname = [];
     end
-    
-    [status,githash] = system(['cd ',location_obj,' && git rev-parse HEAD']);
-    githash = strtrim(githash);
-    
-    if status || ~all(ismember(lower(githash),'0123456789abcdef'))
-        warning(['Result does not seem to be a valid git hash: ''%s''' , ...
-            newline , '   input  = ''%s''' , ...
-            newline , '   target = ''%s''' ...
-            ],githash,objname,location_obj);
+    if isempty(objname)
+        objname = pwd;
+        warning('Input empty, getting githash for pwd: "%s".',objname);
+    end
+
+    try
+        assert(ischar(objname) || isstring(objname),'''objname'' must be name of folder, file, or function in search path.');
+        if isfolder(objname)
+            D = dir(objname);
+            location_obj = D(1).folder;
+        else
+            location_obj = fileparts(which(objname));
+            assert(isfolder(location_obj),['Cmd "fileparts(which(''',objname,'''))" did not return a valid folder.' , newline , ...
+                'Returned "' , location_obj '" instead.']);
+        end
+
+        [status,githash] = system(['cd ',location_obj,' && git rev-parse HEAD']);
+        githash = strtrim(githash);
+
+        if status || ~all(ismember(lower(githash),'0123456789abcdef'))
+            warning(['Result does not seem to be a valid git hash: ''%s''' , ...
+                newline , '   input  = ''%s''' , ...
+                newline , '   target = ''%s''' ...
+                ],githash,objname,location_obj);
+        end
+    catch ME
+        githash = ['misc.githash error: "' , ME.message , '"'];
     end
     
 end
