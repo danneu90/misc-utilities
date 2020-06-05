088713edb63adc7fa6fdce052dd41e472cb29b1a
MWMIMO <daniel.neunteufel@tuwien.ac.at>
2020-06-05 23:15:19 +0200

    num_parser speedup

--- +misc/num_parser.m
+++ +misc/num_parser.m
@@ -7,14 +7,22 @@ function num = num_parser(strarray)
     neg = strarray.startsWith('-');
     strarray = strarray.strip('left','-');
     
-    num = arrayfun(@(x) my_str2num(x), strarray);
+    if all(strarray.lower.startsWith('0x'))
+        num = hex2dec(strarray.extractAfter(2));
+    else
+        num = arrayfun(@(x) my_str2num(x), strarray);
+    end
     num(neg) = -num(neg);
     
 end
 
 function num = my_str2num(str)
 
-    num = double(str2num(str));
+    if str.lower.startsWith('0x')
+        num = hex2dec(str.extractAfter(2));
+    else
+        num = double(str2num(str));
+    end
     if isempty(num)
         num = nan;
     end
