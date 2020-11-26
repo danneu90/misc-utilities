589ec0d1ce5b6b3c9ff52dc04874717a928be204
mwmimo <daniel.neunteufel@tuwien.ac.at>
2020-11-26 13:38:39 +0100

    fixed challenge_me

--- +misc/challenge_me.m
+++ +misc/challenge_me.m
@@ -1,11 +1,19 @@
 function status = challenge_me(level)
 %status = misc.challenge_me(level)
+% status true if passed, false otherwise.
+% level is number of letters to be asked
     if nargin < 1
         level = 1;
     else
         level = max(1,level);
     end
+    text = 'For security reasons, please enter the letter';
+    if level > 1
+        text = [text,'s'];
+    end
     challenge_letter = char(96+randi(26,1,level));
-    status = strcmp(challenge_letter,input(sprintf('For security reasons, please enter the letter(s) ''%s'': ',challenge_letter),'s'));
-    assert(status,'Consider being more careful next time!');
+    status = strcmp(challenge_letter,input(sprintf('%s ''%s'': ',text,challenge_letter),'s'));
+    if ~status
+        warning('Consider being more careful next time!');
+    end
 end
