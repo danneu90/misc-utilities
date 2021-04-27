33375717b21368e7bffa30b9af0e58730e30d76f
Daniel Neunteufel <daniel.neunteufel@tuwien.ac.at>
2021-04-27 15:55:45 +0200

    vonMises circvar2kappa rewritten

--- +mysp/log_besseli.m
+++ +mysp/log_besseli.m
@@ -3,16 +3,16 @@ function lbi = log_besseli(nu,z)
 %
 % Calculate log of the modified Bessel function of first kind.
 % For large z (> 800), a third order approximation is used.
-    
+
     assert(isscalar(nu),'nu must be scalar.');
 
     log_besseli_approx = @(z) z - 1/2*log(2*pi*z) + log( 1 ...
                                                     - (4*nu^2-1)./(8*z) ...
                                                     + (4*nu^2-1)*(4*nu^2-9)./(2*8^2*z.^2) ...
                                                     - (4*nu^2-1)*(4*nu^2-9)*(4*nu^2-25)./(6*8^3*z.^3));
-    
+
     lbi = log(besseli(nu,z));
-    
+
     is_out_of_bound = isinf(lbi);
 
     if any(is_out_of_bound) 
